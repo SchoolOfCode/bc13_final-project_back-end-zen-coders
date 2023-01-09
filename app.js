b28eb54557b9733c1a45d5e2ead39cff17cdd363
Bella Bekanova <108465697+BellaBekanova@users.noreply.github.com>
2023-01-09 21:13:17 +0400

    created models and CRUD routes for ratings

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import { mdbConnect } from "./db/db.js";
 
 import usersRouter from "./routes/users.js";
+import ratingsRouter from "./routes/rating.js"
 
 const app = express();
 const PORT = process.env.PORT || 3002;
@@ -16,6 +17,8 @@ mdbConnect();
 app.use(express.json());
 
 app.use("/users", usersRouter);
+app.use("/ratings", ratingsRouter);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
