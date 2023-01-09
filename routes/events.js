3933c9441a799716b98a5692141aabda531baa28
Ilwad Kamal <112329600+ilwadk@users.noreply.github.com>
2023-01-09 16:04:35 +0000

    events router complete

--- routes/events.js
+++ routes/events.js
@@ -27,23 +27,63 @@ router.post("/add", async (req, res) => {
    const eventPic = req.body.eventPic
   
     // "new" creates a new instance of an  object
-    const newUser = new Event({
-        title:
-        skill: 
-        location: 
-        area: 
-        description: 
-        startTime: 
-        sharerId
-        eventPic
+    const newEvent = new Event({
+        title,
+        skill,
+        location,
+        area,
+        description,
+        startTime,
+        sharerId,
+        eventPic,
 
     });
   
     try {
-      const result = await newUser.save(); //save method allows the new use to be saved in mdb
-      res.json("User added!");
+      const result = await newEvent.save(); //save method allows the new use to be saved in mdb
+      res.json("Event added!");
     } catch (error) {
       console.log(error);
+      res.status(400).json("Error: " + error);
+    }
+  });
+
+  router.get("/:id", async (req, res) => {
+    try {
+      //findbyid is a mongodb method
+      const result = await Event.findById(req.params.id);
+      res.json(result);
+    } catch (error) {
+      console.log("Error: " + error);
+      res.status(400).json("Error: " + error);
+    }
+  });
+
+  router.delete("/:id", async (req, res) => {
+    try {
+      const result = await Event.findByIdAndDelete(req.params.id);
+      res.json("Event deleted");
+    } catch (error) {
+      console.log("Error: " + error);
+      res.status(400).json("Error: " + error);
+    }
+  });
+
+  router.patch("/update/:id", async (req, res) => {
+    try {
+      const event = await Event.findById(req.params.id);
+      event.title= req.body.title
+      event.skill = req.body.skill
+      event.location = req.body.location 
+      event.area = req.body.area
+      event.description = req.body.description
+      event.startTime = req.body.startTime
+      event.sharerId = req.body.sharerId
+      event.eventPic = req.body.eventPic
+      event.result = await user.save();
+      res.json("Event updated!");
+    } catch (error) {
+      console.log("Error: " + error);
       res.status(400).json("Error: " + error);
     }
   });
