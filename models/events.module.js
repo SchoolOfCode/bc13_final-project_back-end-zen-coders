420ea81079cf5ef909f5be60eb43b1d5da302be7
Bella Bekanova <108465697+BellaBekanova@users.noreply.github.com>
2023-01-20 13:57:42 +0400

    changed area in events schema to required false

--- models/events.module.js
+++ models/events.module.js
@@ -7,7 +7,7 @@ const eventSchema = new Schema(
     title: { type: String, required: true, unique: true },
     skill: { type: String, required: true },
     location: { type: String, required: true },
-    area: { type: String, required: true },
+    area: { type: String, required: false },
     description: { type: String, required: true, maxLength: 6500 },
     startTime: { type: Date, default: Date.now, required: true },
     sharerId: {
